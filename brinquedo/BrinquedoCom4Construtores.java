75026f45b8b4b3c8afef8e35f9a8f84ff99b6d86
FELPSTS <macleimotog0@gmail.com>
2025-04-07 23:55:20 -0300

    java construtores

--- brinquedo/BrinquedoCom4Construtores.java
+++ brinquedo/BrinquedoCom4Construtores.java
@@ -1,31 +1,69 @@
+package construtor;
+
 public class BrinquedoCom4Construtores {
 
     private int codigo;
     private String nome;
     private String descricao;
     private double preco;
+
     public BrinquedoCom4Construtores() {
-        this.nome = "Brinquedo Padrão";
         this.codigo = 0;
+        this.nome = "Brinquedo Padrão";
         this.descricao = "";
         this.preco = 0.0;
     }
+
     public BrinquedoCom4Construtores(int codigo, String nome) {
         this.codigo = codigo;
         this.nome = nome;
         this.descricao = "";
         this.preco = 0.0;
     }
+
     public BrinquedoCom4Construtores(int codigo, String nome, String descricao) {
         this.codigo = codigo;
         this.nome = nome;
         this.descricao = descricao;
         this.preco = 0.0;
     }
+
     public BrinquedoCom4Construtores(int codigo, String nome, String descricao, double preco) {
         this.codigo = codigo;
         this.nome = nome;
         this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(double preco) {
         this.preco = preco;
     }
 }
--- brinquedo/Testeconstrutores.java
+++ brinquedo/Testeconstrutores.java
@@ -1,15 +1,27 @@
+package construtor;
+
 public class Testeconstrutores {
 
     public static void main(String[] args) {
-        Brinquedo brinquedo = new Brinquedo();
-        BrinquedoCom4Construtores brinquedo1 = new BrinquedoCom4Construtores();
-        BrinquedoCom4Construtores brinquedo2 = new BrinquedoCom4Construtores(1001, "Carrinho");
+        
+        BrinquedoCom4Construtores brinquedo1 = new BrinquedoCom4Construtores(); 
+        BrinquedoCom4Construtores brinquedo2 = new BrinquedoCom4Construtores(1001, "Carrinho"); 
         BrinquedoCom4Construtores brinquedo3 = new BrinquedoCom4Construtores(1002, "Boneca", "Boneca de pano");
-        BrinquedoCom4Construtores brinquedo4 = new BrinquedoCom4Construtores(1003, "Quebra-cabeça", "Quebra-cabeça 100 peças", 15.99);
+        BrinquedoCom4Construtores brinquedo4 = new BrinquedoCom4Construtores(1003, "Quebra-cabeça", "Quebra-cabeça 100 peças", 15.99); 
+
+        brinquedo1.setNome("Carrinho de corrida");
+        brinquedo1.setDescricao("Carrinho de brinquedo de corrida");
+        brinquedo1.setPreco(29.99);
+        brinquedo1.setCodigo(101);
+
         System.out.println("\nDetalhes dos brinquedos:");
-        System.out.println("Brinquedo 1: " + brinquedo1.getNome() + ", Idade Mínima: " + brinquedo1.getIdadeMinima() + ", Preço: $" + brinquedo1.getPreco() + ", Fabricante: " + brinquedo1.getFabricante());
-        System.out.println("Brinquedo 2: " + brinquedo2.getNome() + ", Idade Mínima: " + brinquedo2.getIdadeMinima() +", Preço: $" + brinquedo2.getPreco() + ", Fabricante: " + brinquedo2.getFabricante());
-        System.out.println("Brinquedo 3: " + brinquedo3.getNome() + ", Idade Mínima: " + brinquedo3.getIdadeMinima() +", Preço: $" + brinquedo3.getPreco() + ", Fabricante: " + brinquedo3.getFabricante());
-        System.out.println("Brinquedo 4: " + brinquedo4.getNome() + ", Idade Mínima: " + brinquedo4.getIdadeMinima() +", Preço: $" + brinquedo4.getPreco() + ", Fabricante: " + brinquedo4.getFabricante());
+        System.out.println("Brinquedo 1: " + brinquedo1.getNome() + ", Descrição: " + brinquedo1.getDescricao() +
+                ", Preço: $" + brinquedo1.getPreco() + ", Código: " + brinquedo1.getCodigo());
+        System.out.println("Brinquedo 2: " + brinquedo2.getNome() + ", Descrição: " + brinquedo2.getDescricao() +
+                ", Preço: $" + brinquedo2.getPreco() + ", Código: " + brinquedo2.getCodigo());
+        System.out.println("Brinquedo 3: " + brinquedo3.getNome() + ", Descrição: " + brinquedo3.getDescricao() +
+                ", Preço: $" + brinquedo3.getPreco() + ", Código: " + brinquedo3.getCodigo());
+        System.out.println("Brinquedo 4: " + brinquedo4.getNome() + ", Descrição: " + brinquedo4.getDescricao() +
+                ", Preço: $" + brinquedo4.getPreco() + ", Código: " + brinquedo4.getCodigo());
     }
 }
